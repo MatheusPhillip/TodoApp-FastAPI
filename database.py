b776b5476dd9d97b0fbcd80f9ef07d2d3586b6c9
Matheus Phillip <mpssm@cesar.org.br>
2024-12-17 18:28:28 -0300

    add new database, docker, and requirements.txt

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://phillip:1234@localhost:5000/todoapp-db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
