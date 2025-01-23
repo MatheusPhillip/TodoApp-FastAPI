0454706476aa03e7d15091bc76ba301bd898b496
Matheus Phillip <mpssm@cesar.org.br>
2025-01-23 08:08:20 -0300

    users, auth and admin tests

--- routers/todos.py
+++ routers/todos.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from .auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/todos',
+    tags=['todos']
+)
 
 def get_db():
     db = SessionLocal()
